c2a4eb121e0825101d60b2526162993bddad4fc5
testingworldnoida <testingworldnoida@gmail.com>
2018-05-19 11:01:23 +0530

    TASK-112 : Automation Project Started

---
+++ Automation/src/testing/A.java
@@ -0,0 +1,23 @@
+package testing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class A {
+	
+	public static void main(String[] args) throws IOException {
+		
+		File f = new File("E:/AR12.txt");
+		FileWriter fw = new FileWriter(f);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("Hello World");
+		bw.flush();
+		bw.close();
+	}
+
+}
